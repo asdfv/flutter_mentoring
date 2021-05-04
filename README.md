a4781f5e9b2e3d3bfca4ec526c6fed9830fabddf
Vasili <vasili.shkleinik@ah.nl>
2021-05-04 21:49:28 +0300

    Add google maps screen

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ One main screen with 3 tabs:
 1st - WebView with any web-page loaded
 2nd - MapView with one market (random location)
 3rd - VideoPlayer with any video in it.
+
+> :warning: add your google API key as local.property field for Android `google.map.key=`
--- lib/presentation/map_view_page.dart
+++ lib/presentation/map_view_page.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class MapViewPage extends StatelessWidget {
+class MapViewPage extends StatefulWidget {
+
+  @override
+  _MapViewPageState createState() => _MapViewPageState();
+}
+
+class _MapViewPageState extends State<MapViewPage> {
+  GoogleMapController mapController;
+
+  final LatLng _center = const LatLng(45.521563, -122.677433);
+
+  void _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("MapView")));
+    return MaterialApp(
+      home: Scaffold(
+        body: GoogleMap(
+          onMapCreated: _onMapCreated,
+          initialCameraPosition: CameraPosition(
+            target: _center,
+            zoom: 11.0,
+          ),
+        ),
+      ),
+    );
   }
 }
