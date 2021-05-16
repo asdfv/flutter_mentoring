f4350cc151b37d69d07d19919f154b366b9519a1
Vasili <vasili.shkleinik@ah.nl>
2021-05-16 13:18:07 +0300

    batteryChannel -> platformNameChannel

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -4,16 +4,16 @@ import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-    
+
     override func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
     ) -> Bool {
         let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-        
-        let batteryChannel = FlutterMethodChannel(name: "by.grodno.vasili.flutter_mentoring/platformName",
+
+        let platformNameChannel = FlutterMethodChannel(name: "by.grodno.vasili.flutter_mentoring/platformName",
                                                   binaryMessenger: controller.binaryMessenger)
-        batteryChannel.setMethodCallHandler({
+        platformNameChannel.setMethodCallHandler({
             [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
             guard call.method == "getPlatformName" else {
                 result(FlutterMethodNotImplemented)
@@ -22,12 +22,12 @@ import GoogleMaps
             let version = UIDevice.current.systemVersion
             result("iOS " + version)
         })
-        
-        
+
+
         GeneratedPluginRegistrant.register(with: self)
-        
+
         GMSServices.provideAPIKey("AIzaSyBTUH2dhArTcqaErMpHxBNxf2MdHeOqqFo")
-        
+
         return super.application(application, didFinishLaunchingWithOptions: launchOptions)
     }
 }
